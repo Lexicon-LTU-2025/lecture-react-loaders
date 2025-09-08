9f97e12a4a382b99cf4fa314da7d0443a004d4bd
Niklas Fähnrich <niklas.fahnrich@safeture.com>
2025-09-08 09:52:03 +0200

    add comments

--- src/views/HomeView.tsx
+++ src/views/HomeView.tsx
@@ -1,12 +1,16 @@
 import type { ReactElement } from 'react';
-import { NavLink } from 'react-router';
+import { NavLink, useNavigation } from 'react-router';
+import { Spinner } from '../components';
 
 export const HomeView = (): ReactElement => {
+  // Returns the currenct navigation object. It includes the state of the navigation which we can use to render som UI when the navigation is 'ongoing'.
+  const { state } = useNavigation();
+
   return (
     <main id="home-view">
       <button className="link-btn">
         <NavLink className="link" to="/random-blocking">
-          To Random Cocktail View - Blocking
+          {state === 'loading' ? <Spinner /> : 'To Random Cocktail View - Blocking'}
         </NavLink>
       </button>
     </main>
