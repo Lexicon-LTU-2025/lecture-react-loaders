24da8387e41776c01307d922f90289d2156e34f1
Niklas Fähnrich <niklas.fahnrich@safeture.com>
2025-09-10 11:52:11 +0200

    add tools for updateing the url with search param name

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,11 +1,32 @@
+import { useRef } from 'react';
+import { useSearchParams } from 'react-router';
+
 export const SearchForm = () => {
-  return <form id="search-form">
-    <fieldset>
-      <legend>Cocktail Parameters</legend>
-      <div>
-        <label htmlFor="name">Name</label>
-        <input id="name" type="text" />
-      </div>
-    </fieldset>
-  </form>;
+  const [searchParams, setSearchParams] = useSearchParams();
+  const nameRef = useRef<HTMLInputElement>(null);
+
+  const handleOnChange = () => {
+    if (!nameRef.current) return;
+    setSearchParams({ name: nameRef.current.value });
+  };
+
+  const name = searchParams.get('name') ?? ''; // Nullish coalescing - fallback value in simple terms.
+
+  return (
+    <form id="search-form">
+      <fieldset>
+        <legend>Cocktail Parameters</legend>
+        <div>
+          <label htmlFor="name">Name</label>
+          <input
+            defaultValue={name}
+            id="name"
+            onChange={handleOnChange}
+            ref={nameRef}
+            type="text"
+          />
+        </div>
+      </fieldset>
+    </form>
+  );
 };
